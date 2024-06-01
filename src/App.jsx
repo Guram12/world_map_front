3159cda7767e1e5690a56b5fd382029114d4ed96
Guram12 <guram.shanidze.33@gmail.com>
2024-06-01 15:53:39 +0400

    zooming  lybrary ds has been added and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Map from './components/Map'
-
-
+import Map2 from './components/Map2'
+import Map_copy from './components/Map_copy'
 
 
 
@@ -10,8 +10,12 @@ function App() {
 
 
   return (
-    <div  >
-      <Map />
+    <div id='main_app_container' >
+      {/* <Map /> */}
+      <div style={{overflow: "hidden"}} >
+      <Map_copy/>
+
+      </div>
     </div>
   )
 }
