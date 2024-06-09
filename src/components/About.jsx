6831a49543a159043093674bb7add8e4944a113d
nino <nino@ninos-MacBook-Air.local>
2024-06-09 17:05:12 +0400

    add two pages, about pages and contact pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,112 @@
 import React from "react";
+import Logo from "../asset/logoo.svg";
+import "../styles/About.css";
+import { motion } from "framer-motion";
+import { fadeIn } from "../Variants";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".paragraph").style.display = "none";
+    const logo = document.querySelector(".logo_image");
+    logo.classList.add("grow-logo");
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  };
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about_container">
+      <motion.h1
+        variants={fadeIn("down", 0.3)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.3 }}
+        className="title"
+      >
+        About Us
+      </motion.h1>
+      <div className="title_and_paragraph">
+        <motion.div
+          variants={fadeIn("right", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="paragraph"
+        >
+          <p>
+            This setup will move the Contact Us element to the center of the
+            screen and scale it up when clicked, then navigate to the contact
+            page after the animation completes.
+          </p>
+          <div className="para_span">
+            <p className="tit">Our mission</p>
+            <span>
+              This setup will move the Contact Us element to the center of the
+              screen and scale it up when clicked, then navigate to the contact
+              page after the animation completes.
+            </span>
+          </div>
+          <div className="para_span">
+            <p className="tit">Our mission</p>
+            <span>
+              This setup will move the Contact Us element to the center of the
+              screen and scale it up when clicked, then navigate to the contact
+              page after the animation completes.
+            </span>
+          </div>
+          <div className="para_span">
+            <p className="tit">Our mission</p>
+            <span>
+              . This setup will move the Contact Us element to the center of the
+              to the contact page after the animation
+            </span>
+            <span>
+              . This setup will move the Contact Us element to the center of the
+              to the contact page after the animation
+            </span>
+            <span>
+              . This setup will move the Contact Us element to the center of the
+              to the contact page after the animation
+            </span>
+          </div>
+          <div className="para_span">
+            <p className="tit">Our mission</p>
+            <span>
+              . This setup will move the Contact Us element to the center of the
+              to the contact page after the animation
+            </span>
+            <span>
+              . This setup will move the Contact Us element to the center of the
+              to the contact page after the animation
+            </span>
+            <span>
+              . This setup will move the Contact Us element to the center of the
+              to the contact page after the animation
+            </span>
+          </div>
+          <div className="para_span">
+            <p className="tit">Our mission</p>
+            <span>
+              This setup will move the Contact Us element to the center of the
+              screen and scale it up when clicked, then navigate to the contact
+              page after the animation completes.
+            </span>
+          </div>
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="logo_image"
+          onClick={handleLogoClick}
+        >
+          <img src={Logo} alt=""></img>
+        </motion.div>
+      </div>
     </div>
   );
 };
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,112 @@
 import React from "react";
+import "../styles/Contact.css";
+import Logo from "../asset/logoo.svg";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { fadeIn } from "../Variants";
+import Inst from "../asset/instagram.png";
+import Face from "../asset/facebook.png";
+import Linke from "../asset/linkedin.png";
 
 const Contact = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClickContact = () => {
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".paragraph_contact").style.display = "none";
+    document.querySelector(".social_links").style.display = "none";
+    const logo = document.querySelector(".logo_image_contact");
+    logo.classList.add("grow-logo-cont");
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  };
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contact_container">
+      <motion.h1
+        variants={fadeIn("down", 0.3)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.3 }}
+        className="title"
+      >
+        Contact Us
+      </motion.h1>
+      <div className="title_and_paragraph_contact">
+        <motion.div
+          variants={fadeIn("right", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="logo_image_contact"
+          onClick={handleLogoClickContact}
+        >
+          <img className="logo_img_cont" src={Logo} alt=""></img>
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="paragraph_contact"
+        >
+          <p>
+            This setup ensures that each text block animates down from the top
+            sequentially, with a one-second delay between each. The title will
+            appear last, after all other elements. This setup ensures that each
+            text block animates down from the top sequentially, with a
+            one-second delay between each. The title will appear last, after all
+            other elements.
+          </p>
+          <div className="contact_info">
+            <p>
+              . Email:<span>robakidzenino89@mail.com</span>
+            </p>
+            <p>
+              . Phone:<span>577424310</span>
+            </p>
+            <p>
+              . Address:<span>Mtatsminda, Vedzini #9</span>
+            </p>
+          </div>
+          <p>
+            This setup ensures that each text block animates down from the top
+            sequentially, with a one-second delay between each. The title will
+            appear last, after all other elements. This setup ensures that each
+            text block animates down from the top sequentially, with a
+            one-second delay between each. The title will appear last, after all
+            other elements.
+          </p>
+          {/* <div className="social_links">
+            <a href="">
+              <img src={Inst} alt=""></img>
+            </a>
+            <a href="">
+              <img src={Face} alt=""></img>
+            </a>
+            <a href="">
+              <img src={Linke} alt=""></img>
+            </a>
+          </div> */}
+        </motion.div>
+      </div>
+      <motion.div
+        variants={fadeIn("up", 0.3)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.3 }}
+        className="social_links"
+      >
+        <a href="">
+          <img src={Inst} alt=""></img>
+        </a>
+        <a href="">
+          <img src={Face} alt=""></img>
+        </a>
+        <a href="">
+          <img src={Linke} alt=""></img>
+        </a>
+      </motion.div>
     </div>
   );
 };
