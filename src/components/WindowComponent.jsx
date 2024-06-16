9e0e45916a7a7cfb105ceed392f7f1afd13853e0
Guram12 <guram.shanidze.33@gmail.com>
2024-06-16 20:05:38 +0400

    add second click map opening

--- src/components/WindowComponent.jsx
+++ src/components/WindowComponent.jsx
@@ -1,13 +1,7 @@
 import "../styles/WindowComponent.css";
-import { useEffect } from "react";
 
 
-
-export default function WindowComponent({ selectedCountry, countryData }) {
-
-
-
-
+export default function WindowComponent({ selectedCountry, countryData , country_name_props }) {
 
   const getCountryCode = (code) => {
     if (code.includes('_')) {
@@ -19,26 +13,20 @@ export default function WindowComponent({ selectedCountry, countryData }) {
     return code;
   };
 
-  // window.open(countryMapUrl, "_blank", "noopener,noreferrer");
-
-
   const handle_Open_Map_Click = (arg) => {
     const countryMapUrl = `${window.location.origin}/country-map/${arg}`;
     window.open(countryMapUrl, "_blank", "noopener,noreferrer");
   }
 
-
-  useEffect(() => {
-    console.log(countryData);
-  }, [countryData]);
-
   const countryCode = getCountryCode(selectedCountry);
 
   return (
     <div className="image_window">
-        <img src={`https://flagsapi.com/${countryCode}/flat/64.png`} alt="flag image"  style={{width: "100px"}}/>
+      <img src={`https://flagsapi.com/${countryCode}/flat/64.png`} alt="flag image" style={{ width: "100px" }} />
       {/* <img src={countryData[countryCode]?.image_url} alt="costumer image" /> */}
-      <button onClick={() => handle_Open_Map_Click(selectedCountry)} className="open_map"  > go to map   </button>
+      <p className="country_name" >{country_name_props}</p>
+
+      <button onClick={() => handle_Open_Map_Click(selectedCountry)} className="open_map"  > Go to map </button>
     </div>
   );
 }
