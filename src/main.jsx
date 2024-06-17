84ae0127be179caac2075db7acc62b7f940dcb24
Guram12 <guram.shanidze.33@gmail.com>
2024-06-17 14:36:47 +0400

    add gtag

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+
+const loadGTag = () => {
+  const gtagId = import.meta.env.VITE_G_TAG_ID; 
+
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', gtagId);
+  };
+  document.head.appendChild(script);
+  
+  // Initializing GTM data layer and configuration
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', gtagId);
+};
+
+loadGTag();
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
