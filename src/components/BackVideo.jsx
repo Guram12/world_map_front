cf1c109938d07a9649a3ecd435574c1e908591be
nino <nino@ninos-MacBook-Air.local>
2024-06-19 19:02:30 +0400

    test

--- src/components/BackVideo.jsx
+++ src/components/BackVideo.jsx
@@ -1,22 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "../styles/BckVideo.css";
 
 const BackgroundVideo = ({ src }) => {
+  const videoRef = useRef(null);
+
   useEffect(() => {
-    const videoElement = document.querySelector(".background-video");
+    const videoElement = videoRef.current;
     if (videoElement) {
-      videoElement.blur();
+      videoElement.removeAttribute("controls");
+      videoElement.blur(); // Ensure the video does not focus on load
     }
   }, []);
 
   return (
     <video
+      ref={videoRef}
       autoPlay
       loop
       muted
       playsInline
       className="background-video"
-      controls={false}
     >
       <source src={src} type="video/mp4" />
     </video>
